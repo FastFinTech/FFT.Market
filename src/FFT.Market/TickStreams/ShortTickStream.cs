c656858b6bfa0ed927bc7c94ec55eaf93ae6a19b
True Goodwill <true@truegoodwill.com>
2021-03-19 19:03:42 -0400

    Improved ShortTickStream

--- src/FFT.Market/TickStreams/ShortTickStream.cs
+++ src/FFT.Market/TickStreams/ShortTickStream.cs
@@ -12,12 +12,12 @@ namespace FFT.Market.TickStreams
   using MessagePack;
   using Nerdbank.Streams;
 
-  public class ShortTickStream : ITickStream, IDisposable
+  public sealed class ShortTickStream : ITickStream, IDisposable
   {
     private readonly double _tickSize;
     private readonly Sequence<byte> _sequence;
 
-    private Tick _previousTick;
+    private Tick? _previousTick;
 
     public ShortTickStream(TickStreamInfo info)
     {
@@ -32,6 +32,10 @@ namespace FFT.Market.TickStreams
       _sequence = new Sequence<byte>(ArrayPool<byte>.Shared);
       _sequence.Write(bytes);
       Info = info;
+      DataLength = _sequence.Length;
+      var reader = CreateReader();
+      for (var tick = reader.ReadNext(); tick is not null; tick = reader.ReadNext())
+        _previousTick = tick;
     }
 
     public ShortTickStream(TickStreamInfo info, Sequence<byte> sequence)
@@ -39,16 +43,23 @@ namespace FFT.Market.TickStreams
       _tickSize = info.Instrument.TickSize;
       _sequence = sequence;
       Info = info;
+      DataLength = _sequence.Length;
+      var reader = CreateReader();
+      for (var tick = reader.ReadNext(); tick is not null; tick = reader.ReadNext())
+        _previousTick = tick;
     }
 
+    /// <inheritdoc />
     public TickStreamInfo Info { get; }
-    public int Count { get; private set; }
-    public long DataLength => _sequence.Length;
 
+    /// <inheritdoc />
+    public long DataLength { get; private set; }
+
+    /// <inheritdoc />
     public void WriteTick(Tick tick)
     {
       var writer = new MessagePackWriter(_sequence);
-      if (Count == 0)
+      if (_previousTick is null)
       {
         writer.Write(tick.Price);
         writer.Write(tick.Bid);
@@ -67,21 +78,35 @@ namespace FFT.Market.TickStreams
 
       _previousTick = tick;
 
-      // NB: Increment the count property last, after all bytes are written, so that multi-threaded reading, which checks the Count property,
-      // doesn't start reading a tick before all fields are completely written to the byte buffer.
-      Count++;
+      // NB: Increment the DataLength property last, after all bytes are
+      // written, so that multi-threaded reading, which checks the DataLength
+      // property, doesn't start reading a tick before all fields are completely
+      // written to the sequence.
+      DataLength = _sequence.Length;
     }
 
+    /// <summary>
+    /// Gets a ReadOnlySequence for the underlying data. The ReadOnlySequence
+    /// will only be valid until instance is disposed, when the underlying
+    /// buffers are recycled to their memory pool. Call this method again to get
+    /// new data when more ticks are written to the stream.
+    /// </summary>
+    /// <remarks>Don't call this method multi-threaded with the WriteTick
+    /// method, as you may receive a sequnce that has a partially-written tick
+    /// at the end.</remarks>
     public ReadOnlySequence<byte> AsReadOnlySequence()
       => _sequence.AsReadOnlySequence;
 
+    /// <inheritdoc />
     public ITickStreamReader CreateReader()
       => new Reader(this);
 
+    /// <inheritdoc />
     public ITickStreamReader CreateReaderFrom(TimeStamp timeStamp)
     {
+      // This will EXCLUDE ticks at exactly "timeStamp".
       var reader = CreateReader();
-      reader.ReadUntilJustBefore(timeStamp).Count(); // don't forget to actually enumerate the enumerable with .Count() or similar
+      reader.ReadUntil(timeStamp).Count(); // don't forget to actually enumerate the enumerable with .Count() or similar
       return reader;
     }
 
@@ -95,7 +120,7 @@ namespace FFT.Market.TickStreams
       private readonly ShortTickStream _parent;
       private readonly double _tickSize;
 
-      private long _position;
+      private long _position = 0;
       private int _currentIndex = -1;
       private int _peekIndex = -1;
       private Tick _peekTick;
@@ -104,19 +129,19 @@ namespace FFT.Market.TickStreams
       public Reader(ShortTickStream parent)
       {
         _parent = parent;
-        Info = _parent.Info;
         _tickSize = _parent._tickSize;
+        Info = _parent.Info;
       }
 
       public TickStreamInfo Info { get; }
 
-      public long BytesRemaining => _parent._sequence.Length - _position;
+      public long BytesRemaining => _parent.DataLength - _position;
 
       public Tick? PeekNext()
       {
         var index = _currentIndex + 1;
         if (_peekIndex == index) return _peekTick;
-        if (index >= _parent.Count) return null;
+        if (BytesRemaining == 0) return null;
         _peekTick = Extract();
         _peekIndex = index;
         return _peekTick;
@@ -131,7 +156,7 @@ namespace FFT.Market.TickStreams
           return _peekTick;
         }
 
-        if (index >= _parent.Count)
+        if (BytesRemaining == 0)
           return null;
 
         _currentIndex = index;
