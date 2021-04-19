46f47a44b34bc1081455e35a4c15c438dae0c9d3
True Goodwill <true@truegoodwill.com>
2021-04-18 22:09:44 -0400

    Tests: Minor updates

---
+++ src/FFT.Market.Tests/Instrument.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) True Goodwill. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace FFT.Market.Tests
+{
+  using FFT.Market.Instruments;
+  using FFT.TimeStamps;
+
+  public partial class ShortTickStreamTests
+  {
+    internal sealed record Instrument : IInstrument
+    {
+      public string Name { get; init; }
+      public Asset BaseAsset { get; init; }
+      public Asset QuoteAsset { get; init; }
+      public Exchange Exchange { get; init; }
+      public SettlementTime SettlementTime { get; init; }
+      public double MinPriceIncrement { get; init; }
+      public double MinQtyIncrement { get; init; }
+
+      public bool IsTradingDay(DateStamp date) => true;
+
+      public DateStamp ThisOrNextTradingDay(DateStamp date) => date;
+
+      public DateStamp ThisOrPreviousTradingDay(DateStamp date) => date;
+    }
+  }
+}
